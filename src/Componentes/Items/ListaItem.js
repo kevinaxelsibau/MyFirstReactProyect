7a1c7bdbef5c5e44256bd1ba5c540ff8b861fcbc
kevinaxelsibau <kevinaxelsibau@gmail.com>
2022-02-02 16:23:20 -0300

    Changes

--- src/Componentes/Items/ListaItem.js
+++ src/Componentes/Items/ListaItem.js
@@ -5,30 +5,80 @@ import CostosFiltros from "../Costos/CostosFiltros.js";
 import { useState } from "react/cjs/react.development";
 
 const ListaItem = (props) => {
-    const [filteredYear, setFilteredYear] = useState("2020");
+    const [filteredYear, setFilteredYear] = useState("2021");
 
     const FilterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
-    return (
+
+    const FilterExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+   return (
         <div>
             <Card className="expenses">
                 <CostosFiltros selected={filteredYear}
                     onChangeFilter={FilterChangeHandler}
                 />
-                {props.items.map((expense) =>
-                    expense.date.getFullYear().toString() === filteredYear ? (
-                        <ItemCaro 
-                        key={expense.id} 
-                        title={expense.title} 
-                        amount={expense.amount} 
-                        date={expense.date} />
-                    ) : null
-                )} 
-
+                {FilterExpenses.length === 0 ? (<p>Not Expenses Found.</p>
+                ) : (                
+                FilterExpenses.map((expense) =>
+                        <ItemCaro
+                            key={expense.id}
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date} />
+                ))}
+                
             </Card>
         </div>
     );
 };
 
 export default ListaItem
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     // return (
+//     //     <div>
+//     //         <Card className="expenses">
+//     //             <CostosFiltros selected={filteredYear}
+//     //                 onChangeFilter={FilterChangeHandler}
+//     //             />
+//     //             {/* {props.items.map((expense) =>
+//     //                 expense.date.getFullYear().toString() === filteredYear ? (
+//     //                     <ItemCaro
+//     //                         key={expense.id}
+//     //                         title={expense.title}
+//     //                         amount={expense.amount}
+//     //                         date={expense.date} />
+//     //                 ) : null
+//     //             )} */}
+                
+
+
+
+
+
+//             </Card>
+//         </div>
+//     );
+// };
+
+// export default ListaItem
